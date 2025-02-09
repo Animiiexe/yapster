5d657f671d3ac0caae728ffd099119634d41ec92
Animesh.MS <123883011+Animiiexe@users.noreply.github.com>
2025-02-09 11:01:01 +0000

    server execution

---
+++ client/app/page.jsx
@@ -0,0 +1,45 @@
+"use client";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { HeroUIProvider } from "@heroui/react";
+import { Messages, Inputs, SignUp } from "@/components";
+
+const socket = io(
+  "https://effective-space-umbrella-g9w95pgppj7cwjjv-8000.app.github.dev/"
+);
+
+console.log(socket);
+
+export default function Home() {
+  const [user, setUser] = useState(null);
+  const [imgSrc, setImgSrc] = useState("https://placehold.co/400");
+
+  useEffect(() => {
+    const session = sessionStorage.getItem("user");
+    if (session) {
+      setUser(session);
+    }
+  }, []);
+
+  useEffect(() => {
+    socket.on("image", (data) => {
+      setImgSrc(data);
+    });
+  }, [imgSrc, setImgSrc]);
+
+  return (
+    <HeroUIProvider>
+      <div className="min-h-screen max-h-screen">
+        {!user ? (
+          <SignUp setUser={setUser} socket={socket} />
+        ) : (
+          <div className="relative min-h-screen max-h-screen">
+            <Messages />
+            <img src={imgSrc} />
+            <Inputs socket={socket} />
+          </div>
+        )}
+      </div>
+    </HeroUIProvider>
+  );
+}
--- client/components/SignUp.jsx
+++ client/components/SignUp.jsx
@@ -6,9 +6,26 @@ import {
   Divider,
   Link,
   Image,
+  Button,
+  Form,
+  Input,
 } from "@heroui/react";
+import { ChevronRightIcon } from "lucide-react";
 
-export default function SignUp() {
+export default function SignUp({ setUser, socket }) {
+  const onSubmit = (e) => {
+    // Prevent default browser page refresh.
+    e.preventDefault();
+
+    // Get form data as an object.
+    const data = Object.fromEntries(new FormData(e.currentTarget));
+
+    // Submit data to your backend API.
+    setUser(data.name);
+
+    sessionStorage.setItem("user", data.name);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center ">
       <Card className="max-w-[300px]">
@@ -22,19 +39,37 @@ export default function SignUp() {
           />
           <div className="flex flex-col">
             <p className="text-md">MADE Room</p>
-            <p className="text-small text-default-500">Yapster.chat.us</p>
+            <p className="text-small text-default-500">made.phleebs.tech</p>
           </div>
         </CardHeader>
+
         <Divider />
+
         <CardBody>
-          <p>Make beautiful websites regardless of your design experience.</p>
+          <Form onSubmit={onSubmit} validationBehavior="native">
+            <Input
+              isRequired
+              errorMessage="Please enter a name"
+              label="Name"
+              labelPlacement="outside"
+              name="name"
+              placeholder="Enter your name"
+              type="text"
+              autoComplete="off"
+            />
+            <Button type="submit" className="gap-0">
+              Join <ChevronRightIcon />
+            </Button>
+          </Form>
         </CardBody>
+
         <Divider />
+
         <CardFooter>
           <Link
             isExternal
             showAnchorIcon
-            href="https://github.com/Animiiexe/Yapster"
+            href="https://github.com/shantanuuchak/made"
           >
             Visit source code on GitHub.
           </Link>
